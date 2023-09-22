530a443864eb75c056f51ac584b58e828a654212
BhanuThapa <bhanuthapa252@gmail.com>
2023-09-23 00:38:40 +0530

    Build a Banking Application done

--- 17 Build a banking application/script.js
+++ 17 Build a banking application/script.js
@@ -1,15 +1,19 @@
 let acdetails = {
   name: 'Bhanu',
   balance: 700000,
-  depo: (dep) => {
-    balance = this.balance + dep;
-    console.log(this.balance);
+  depo: function (dep) {
+    this.balance = this.balance + dep;
+    console.log(`Balance after deposite ${this.balance}`);
   },
-  with: (wit) => {
-    balance = this.balance - wit;
-    console.log(this.balance);
+  with: function (wit) {
+    if (this.balance <= 0 || this.balance < wit) {
+      console.log('not enough money, check your balance');
+    } else {
+      this.balance = this.balance - wit;
+      console.log(`Balance after withdrawal ${this.balance}`);
+    }
   },
 };
 
-console.log(acdetails);
 acdetails.depo(70);
+acdetails.with(80000);
