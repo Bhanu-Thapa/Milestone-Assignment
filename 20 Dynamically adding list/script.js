355ebeeda8c587b651727f4c1cc9be732b469d1a
BhanuThapa <bhanuthapa252@gmail.com>
2023-10-05 23:42:35 +0530

    Dynamically adding list

--- 20 Dynamically adding list/script.js
+++ 20 Dynamically adding list/script.js
@@ -1,15 +1,24 @@
-let arr = document.querySelectorAll('arr');
+let arr = document.querySelectorAll('.ar');
 let btn = document.querySelector('button');
+let di = document.querySelector('.di');
 let count = 0;
+
 btn.addEventListener('click', () => {
   for (let i = 0; i < arr.length; i++) {
     if (count == i) {
-      console.log(arr[i]);
       count++;
+      arr[i].style.visibility = 'hidden';
+
+      let para = document.createElement('p');
+      para.textContent = arr[i].textContent;
+      di.appendChild(para);
+
       break;
     }
   }
-  if (arr.length <= count) {
-    console.log(`No ITEM left `);
+  if (count == arr.length) {
+    count++;
+  } else if (count > arr.length) {
+    alert('all items added');
   }
 });
